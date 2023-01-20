4aa4fc18db2b74e8e6c3df85affdb9cba13e10f3
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2023-01-20 13:37:54 +0100

    display name, job and info data

--- src/Review.js
+++ src/Review.js
@@ -3,9 +3,19 @@ import people from './data'
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa'
 
 const Review = () => {
+ const [index, setIndex] = useState(0)
+ const {image, name, text, job} = people[index]
   return (
    <div className='review'>
-    
+    <div className='img-container'>
+     <img className='person-img' src={image} alt={name} />
+     <div className='quote-icon'>
+      <FaQuoteRight />
+     </div>
+    </div>
+    <h3 className='author'>{name}</h3>
+    <h4 className='job'>{job}</h4>
+    <p className='info'>{text}</p>
    </div>
   )
 }
