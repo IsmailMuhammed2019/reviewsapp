f776981bade2c03555bc68c3458f209efc42c888
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2023-01-20 13:40:40 +0100

    setup buttons for left and right control

--- src/Review.js
+++ src/Review.js
@@ -6,17 +6,25 @@ const Review = () => {
  const [index, setIndex] = useState(0)
  const {image, name, text, job} = people[index]
   return (
-   <div className='review'>
-    <div className='img-container'>
-     <img className='person-img' src={image} alt={name} />
-     <div className='quote-icon'>
-      <FaQuoteRight />
-     </div>
+    <div className='review'>
+      <div className='img-container'>
+        <img className='person-img' src={image} alt={name} />
+        <div className='quote-icon'>
+          <FaQuoteRight />
+        </div>
+      </div>
+      <h3 className='author'>{name}</h3>
+      <h4 className='job'>{job}</h4>
+      <p className='info'>{text}</p>
+      <div className='button-container'>
+        <button className='prev-btn'>
+          <FaChevronLeft />
+        </button>
+        <button className='next-btn'>
+          <FaChevronRight />
+        </button>
+      </div>
     </div>
-    <h3 className='author'>{name}</h3>
-    <h4 className='job'>{job}</h4>
-    <p className='info'>{text}</p>
-   </div>
   )
 }
 
